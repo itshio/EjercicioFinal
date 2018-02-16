10254db106795f220c9c994d95c8f72f884cc8a2
darkclif <morpheoosmatrix@gmail.com>
2018-02-16 13:21:42 +0100

    solo un activity

--- app/src/main/java/com/example/ejerciciofinal/ReservaActivity.java
+++ app/src/main/java/com/example/ejerciciofinal/ReservaActivity.java
@@ -1,7 +1,9 @@
 package com.example.ejerciciofinal;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 
 public class ReservaActivity extends AppCompatActivity {
@@ -47,5 +49,13 @@ public class ReservaActivity extends AppCompatActivity {
 
 
         }
+    }
+
+    public void clickaceptar (View view){
+
+        Intent form_main =new Intent(getApplicationContext(),MainActivity.class);
+
+        startActivity(form_main);
+
     }
 }
